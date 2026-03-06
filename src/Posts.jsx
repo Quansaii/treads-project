0e92895b3ca141df860d3bfedd7a495bfd7f0cb7
Sayasat Kuandyk <sayasat614@gmail.com>
2026-03-06 13:52:12 +0500

    Added first design

---
+++ src/Posts.jsx
@@ -0,0 +1,15 @@
+export function Posts(){
+    return(
+        <>
+            <div className="post">
+                <div className="avatar-section">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6bgexWcSEbSAvXvk3YIDfijim1W6_ynrCmQ&s" alt="" />
+                </div>
+                <div className="info">
+                    <div className="nickname">katarinablue</div>
+                    <div className="post-text">fasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnklfasfmkasnfoasnfsanfjnasfijnsafonmsalkfnkl</div>
+                </div>
+            </div>
+        </>
+    )
+}
